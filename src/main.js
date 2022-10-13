36a97ec669490a798e3c644cf74d9678baa0e85a
JonatanRoldan <jroldan42@gmail.com>
2022-10-13 00:22:06 -0600

    colors

--- src/main.js
+++ src/main.js
@@ -3,5 +3,8 @@ function camaleon(){
     let y=Math.floor(Math.random()*256);
     let z=Math.floor(Math.random()*256);
     color = `rgb(${x},${y},${z})`;
-    let bg=document.getElementById('contenedor').style.background=color; 
+    texto=document.getElementById("ColorString")
+    texto.textContent=color;
+    texto.style.color = `rgb(${z},${x},${y})`;
+    document.getElementById('contenedor').style.background=color; 
 }
